0a59df456ddc09b0f271d4c8feaf6136f3d2781b
its-a-wrap <patrick_tweed@hotmail.co.uk>
2020-07-14 14:10:26 +0100

    refactor

--- src/tradingPlatform.js
+++ src/tradingPlatform.js
@@ -1,39 +1,4 @@
-import Order from './order.js';
-
-function isMatchedOrder(order, name, price) {
-  return order.getName() === name && order.getPrice() === price;
-}
-
-function processNewOrder(name, amount, price, arrayToCheck, arrayToAddTo) {
-  if (arrayToCheck.length === 0) {
-    arrayToAddTo.push(new Order(name, amount, price));
-    return;
-  }
-
-  let trackedAmount = amount;
-  const elementsToRemove = [];
-
-  for (let i = 0; i < arrayToCheck.length; i++) {
-    if (isMatchedOrder(arrayToCheck[i], name, price)) {
-      if (arrayToCheck[i].getAmount() > trackedAmount) {
-        arrayToCheck[i].reduceAmount(trackedAmount);
-        trackedAmount = 0;
-        break;
-      } else {
-        trackedAmount -= arrayToCheck[i].getAmount();
-        elementsToRemove.push(i);
-      }
-    }
-  }
-
-  elementsToRemove.forEach((element) => {
-    arrayToCheck.splice(element, 1);
-  });
-
-  if (trackedAmount !== 0) {
-    arrayToAddTo.push(new Order(name, trackedAmount, price));
-  }
-}
+import { processNewOrder } from './helpers.js';
 
 export class TradingPlatform {
   constructor() {
